c3e0b8711132c92fdf87231fbfb6d3e9bd055b86
Matt Beaumont <mbeaumont@thefarmersdog.com>
2021-11-25 15:13:34 -0500

    updated shell timezone to EST/EDT

--- zsh/.config/zsh/custom/env.zsh
+++ zsh/.config/zsh/custom/env.zsh
@@ -1,10 +1,9 @@
 export PATH=$HOME/bin/:/usr/local/bin:$HOME/Library/Python/2.7/bin:$PATH
 export NVM_DIR="$HOME/.nvm"
-export TZ=UTC
 export FZF_CTRL_T_COMMAND='ag -l --nocolor --hidden  -g ""'
 export FZF_DEFAULT_COMMAND='ag -l --nocolor --hidden  -g ""'
 . "/usr/local/opt/nvm/nvm.sh"
-
+export TZ=America/New_York
 export LESS='-R'
 export LESSOPEN='|~/.lessfilter %s'
 export TERM=xterm-256color
