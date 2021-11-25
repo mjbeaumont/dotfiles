7b25b97dcd30c2ae0e990054ab003b28f5be4223
Matt Beaumont <mbeaumont@thefarmersdog.com>
2021-11-25 15:17:25 -0500

    better terminal color function

--- zsh/.config/zsh/custom/functions.zsh
+++ zsh/.config/zsh/custom/functions.zsh
@@ -25,11 +25,8 @@ timezsh() {
 }
 
 termcolors() {
-for i in {0..255} ; do
-    printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
-    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
-        printf "\n";
-    fi
-done
+    for i in {0..255}; do
+        printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
+    done
 }
 
