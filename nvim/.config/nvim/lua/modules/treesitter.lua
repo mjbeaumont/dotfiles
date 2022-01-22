fdab8a91bada1413d3e46b49ef18564e3c773cd7
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-01-22 14:24:53 -0500

    got a halfway usable theme going

--- nvim/.config/nvim/lua/modules/treesitter.lua
+++ nvim/.config/nvim/lua/modules/treesitter.lua
@@ -7,13 +7,31 @@ configs.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false,
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
+  },
   autopairs = {
     enable = true,
   },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = true,
   },
   indent = { enable = true, disable = { "yaml" } },
   context_commentstring = {
