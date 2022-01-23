f43826e3bf2ca1d81232d2e16da757af263c28a6
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-01-23 18:09:17 -0500

    added startup screen to neovim

--- nvim/.config/nvim/lua/modules/telescope.lua
+++ nvim/.config/nvim/lua/modules/telescope.lua
@@ -16,6 +16,7 @@ telescope.setup{
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
+        ["<ESC>"] = actions.close,
         ["<C-h>"] = "which_key"
       },
       n = {
