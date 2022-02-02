d329298d416e3f90c3b5b428c7e9eab258d7bb80
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-02-02 07:50:31 -0500

    added keymap to see open buffers in telescope

--- nvim/.config/nvim/lua/modules/keymaps.lua
+++ nvim/.config/nvim/lua/modules/keymaps.lua
@@ -30,6 +30,8 @@ set_km("n", "<Leader>ff", ":lua require('telescope.builtin').find_files()<CR>")
 set_km("n", "<Leader>fp", ":Telescope projects<CR>")
 set_km("n", "<Leader>fg", ":lua require('telescope.builtin').live_grep()<CR>")
 set_km("n", "<Leader>fs", ":lua require('telescope.builtin').treesitter()<CR>")
+set_km("n", "<Leader>fo", ":lua require('telescope.builtin').buffers()<CR>")
+
 set_km("n", "<Leader>gs", ":lua require('telescope.builtin').git_status()<CR>")
 set_km("n", "<Leader>gc", ":lua require('telescope.builtin').git_bcommits()<CR>")
 set_km("n", "<Leader>gb", ":lua require('telescope.builtin').git_branches()<CR>")
