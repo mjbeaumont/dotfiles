1046ae02eb4e05f55cdfb2b40df8f55670124e7c
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-02-21 13:35:52 -0500

    added keymaps for vim-test and script to cwd into correct directory

--- nvim/.config/nvim/lua/modules/vim-test.lua
+++ nvim/.config/nvim/lua/modules/vim-test.lua
@@ -1,55 +1,34 @@
+local set_km = require("modules/utils")
+
+set_km("n", "<Leader>tf", ":TestFile<CR>");
+set_km("n", "<Leader>tn", ":TestNearest<CR>");
+set_km("n", "<Leader>ts", ":TestSuite<CR>");
+set_km("n", "<Leader>tl", ":TestLast<CR>");
+
 vim.g['test#javascript#jest#file_pattern'] = "\\vtest\\.(js|jsx|ts|tsx)$";
+vim.g['test#javascript#jest#executable'] = 'NODE_ENV=test pnpx jest'
+vim.g['test#strategy'] = 'floaterm'
 
-local function getJestTestCmd()
-	local lsputil = require('lspconfig.util')
-	local path = lsputil.path
+local function getJestCwd()
+    local lsputil = require('lspconfig.util')
 
-	-- Our default command
-	local cmd = 'jest'
+    -- path of the current buffer, relative to the cwd
+    local currentBufferFilePath = vim.fn.expand('%:~:.')
 
-	local cwd = vim.loop.cwd()
-	-- path of the current buffer, relative to the cwd
-	local currentBufferFilePath = vim.fn.expand('%:~:.')
-
-	local pkgJsonParentDir = lsputil.find_package_json_ancestor(
-		currentBufferFilePath
-	)
-	local pkgJsonPath = path.join(cwd, pkgJsonParentDir, 'package.json')
-
-	local file = io.open(pkgJsonPath, 'r')
-
-	if file then
-		local run = 'pnpm run'
-
-		local fileContents = file:read('*a')
-		local jsonTable = vim.fn.json_decode(fileContents)
-
-		-- What we're expecting the script command to be
-		local expectedTestCmd = 'test'
-		local testCmd = jsonTable.scripts[expectedTestCmd]
-
-		cmd =  run .. ' --cwd ' .. pkgJsonParentDir .. ' ' .. testCmd
-
-		io.close(file)
-	end
-
-	return cmd
+    local pkgJsonParentDir = lsputil.find_package_json_ancestor(
+        currentBufferFilePath
+    )
+    print(pkgJsonParentDir)
+    return pkgJsonParentDir
 end
 
-_G.setJestSettings = function(package)
-   vim.g['test#javascript#jest#executable'] = 'NODE_ENV=test pnpx jest' 
-   vim.g['test#project_root'] = 'packages/' ..package
-end
-
-_G.setJestCmd = function()
-	vim.g['test#javascript#jest#executable'] = getJestTestCmd()
+_G.setJestCwd = function ()
+    vim.g['test#project_root'] = getJestCwd()
 end
 
 vim.cmd([[
   augroup test
-    autocmd!
-    autocmd BufEnter ~/Code/TFD/website/packages/website/* call v:lua.setJestSettings('website')
-    autocmd BufEnter ~/Code/TFD/website/packages/tosa/* call v:lua.setJestSettings('tosa')
-    " autocmd BufEnter *.tsx,*.ts,*.js,*.jsx call v:lua.setJestCmd()
+  autocmd!
+  autocmd BufEnter *.tsx,*.ts,*.js,*.jsx call v:lua.setJestCwd()
   augroup END
 ]])
