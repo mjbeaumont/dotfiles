960b8149f5cdd794745d84b2707115f3edf7832b
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-02-25 18:21:48 -0500

    update bufferline settings

--- nvim/.config/nvim/lua/modules/bufferline.lua
+++ nvim/.config/nvim/lua/modules/bufferline.lua
@@ -1,4 +1,12 @@
-require("bufferline").setup{
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+   return
+end
+
+local set_km = require("modules.utils")
+
+
+bufferline.setup{
   options = {
     -- NOTE: this plugin is designed with this icon in mind,
     -- and so changing this is NOT recommended, this is intended
@@ -6,3 +14,5 @@ require("bufferline").setup{
     numbers = "ordinal",
   }
 }
+
+set_km("n", "<Leader>bs", ":BufferLinePick<CR>");
