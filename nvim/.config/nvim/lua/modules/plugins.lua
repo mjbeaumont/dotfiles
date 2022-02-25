4472f4f112525c8d50d32e35cb40e450ebf310d0
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-02-25 18:22:59 -0500

    added treesitter text subjects plugin

--- nvim/.config/nvim/lua/modules/plugins.lua
+++ nvim/.config/nvim/lua/modules/plugins.lua
@@ -138,6 +138,7 @@ return packer.startup(function(use)
     use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
     use  "nvim-treesitter/playground"
     use  "nvim-treesitter/nvim-treesitter-textobjects"
+    use  "RRethy/nvim-treesitter-textsubjects"
     use  "JoosepAlviste/nvim-ts-context-commentstring"
 
     -- Automatically set up your configuration after cloning packer.nvim
