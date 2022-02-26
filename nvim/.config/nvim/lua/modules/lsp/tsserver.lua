887d1bf575b6cff19af5dc8aade9568fa2dd38e6
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-02-26 17:34:37 -0500

    setup lsp-installer

--- nvim/.config/nvim/lua/modules/lsp/tsserver.lua
+++ nvim/.config/nvim/lua/modules/lsp/tsserver.lua
@@ -9,17 +9,16 @@ local ts_utils_settings = {
 }
 
 local M = {}
-M.setup = function(on_attach, capabilities)
+M.getOpts = function(on_attach, capabilities)
     local lspconfig = require("lspconfig")
     local ts_utils = require("nvim-lsp-ts-utils")
-    local opts = { silent = true, noremap = true }
 
-    lspconfig["tsserver"].setup({
+    return {
         root_dir = lspconfig.util.root_pattern("package.json"),
         init_options = ts_utils.init_options,
         on_attach = function(client, bufnr)
             if client.config.flags then
-              client.config.flags.allow_incremental_sync = true
+                client.config.flags.allow_incremental_sync = true
             end
             client.resolved_capabilities.document_formatting = false
             client.resolved_capabilities.document_range_formatting = false
@@ -28,16 +27,12 @@ M.setup = function(on_attach, capabilities)
 
             ts_utils.setup(ts_utils_settings)
             ts_utils.setup_client(client)
-
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", ":TSLspRenameFile<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "go", ":TSLspImportAll<CR>", opts)
         end,
         flags = {
             debounce_text_changes = 150,
         },
         capabilities = capabilities,
-    })
+    }
 end
 
 return M
