ec3430ceda29c2a2acd171846b071d35422c4cbf
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-02-27 21:33:30 -0500

    removed unnecessary log statement

--- nvim/.config/nvim/lua/modules/lsp/eslint.lua
+++ nvim/.config/nvim/lua/modules/lsp/eslint.lua
@@ -6,7 +6,6 @@ local M = {
         return {
             root_dir = lspconfig.util.root_pattern(".eslintrc", ".eslintrc.js", "package.json"),
             on_attach = function(client, bufnr)
-                print "eslint specific onattach"
                 client.resolved_capabilities.document_formatting = true
                 on_attach(client, bufnr)
                 vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-l>", ":EslintFixAll<CR>", opts)
