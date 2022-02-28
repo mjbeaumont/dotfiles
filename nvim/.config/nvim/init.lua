3566354e6b9cbce501fa5927fa90e50f79382f8c
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-02-28 10:44:30 -0500

    got prettier/stylua working

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,25 +1,25 @@
-require "modules/autocommands"
-require "modules/functions"
-require "modules/keymaps"
-require "modules/options"
-require "modules/plugins"
-require "modules/treesitter"
-require "modules/telescope"
-require "modules/bufferline"
-require "modules/alpha"
+require("modules/autocommands")
+require("modules/functions")
+require("modules/keymaps")
+require("modules/options")
+require("modules/plugins")
+require("modules/treesitter")
+require("modules/telescope")
+require("modules/bufferline")
+require("modules/alpha")
 -- require "modules/projects"
-require "modules/lsp"
-require "modules/nvim-cmp"
-require "modules/nvim-tree"
-require "modules/gitlinker"
-require "modules/git-blame"
-require "modules/colortheme"
-require "modules/octo"
-require "modules/vim-test"
-require "modules/floaterm"
-require "modules/indent-blankline"
-require "modules/nvim-treehopper"
+require("modules/lsp")
+require("modules/nvim-cmp")
+require("modules/nvim-tree")
+require("modules/gitlinker")
+require("modules/git-blame")
+require("modules/colortheme")
+require("modules/octo")
+require("modules/vim-test")
+require("modules/floaterm")
+require("modules/indent-blankline")
+require("modules/nvim-treehopper")
+require("modules/formatter")
 
-
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
-vim.cmd [[autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab]]
+vim.cmd([[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]])
+vim.cmd([[autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab]])
