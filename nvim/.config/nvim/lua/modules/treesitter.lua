6f8c72aa4456ad02fb6d0c774fdcf8a731efa809
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-23 10:15:04 -0400

    updates for nvim 0.7

--- nvim/.config/nvim/lua/modules/treesitter.lua
+++ nvim/.config/nvim/lua/modules/treesitter.lua
@@ -4,7 +4,27 @@ if not status_ok then
 end
 
 configs.setup({
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = {
+        "bash",
+        "dockerfile",
+        "graphql",
+        "html",
+        "http",
+        "javascript",
+        "jsdoc",
+        "json",
+        "json5",
+        "lua",
+        "php",
+        "prisma",
+        "python",
+        "scss",
+        "todotxt",
+        "typescript",
+        "vim",
+        "vue",
+        "yaml",
+    }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = { "" }, -- List of parsers to ignore installing
     playground = {
