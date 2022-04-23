861664adfe11c6244749f8c67cc903865b1622d2
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-23 10:15:04 -0400

    updates for nvim 0.7

--- nvim/.config/nvim/lua/modules/nvim-cmp.lua
+++ nvim/.config/nvim/lua/modules/nvim-cmp.lua
@@ -98,9 +98,6 @@ cmp.setup({
         behavior = cmp.ConfirmBehavior.Replace,
         select = false,
     },
-    documentation = {
-        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-    },
     experimental = {
         ghost_text = false,
         native_menu = false,
