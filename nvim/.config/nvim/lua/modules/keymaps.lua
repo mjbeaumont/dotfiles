83928229cc6d621ab0a11073c3931b20e51e20cf
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-23 10:14:31 -0400

    more helpful keybindings

--- nvim/.config/nvim/lua/modules/keymaps.lua
+++ nvim/.config/nvim/lua/modules/keymaps.lua
@@ -8,6 +8,8 @@ vim.g.maplocalleader = " "
 -- s + any two keys to jump around document
 -- set_km("n", "s", "<Plug>(easymotion-s2)", { noremap = false })
 
+set_km("n", "Q", "<Nop>")
+
 set_km("n", "<Leader>hl", ":TSHighlightCapturesUnderCursor<CR>")
 
 -- shortcut to jump to beginning/end of line
@@ -24,6 +26,15 @@ set_km("n", "<Leader>sv", ":lua ReloadConfig()<CR>")
 set_km("v", "J", ":m '>+1<CR>gv=gv")
 set_km("v", "K", ":m '<-2<CR>gv=gv")
 
+set_km("v", ">", ">gv")
+set_km("v", "<", "<gv")
+
+-- easy resize windows
+set_km("n", "<C-Up>", ":resize -2<CR>")
+set_km("n", "<C-Down>", ":resize +2<CR>")
+set_km("n", "<C-Left>", ":vertical resize -2<CR>")
+set_km("n", "<C-Right>", ":vertical resize +2<CR>")
+
 set_km("n", "<C-n>", ":lua ToggleNumber()<CR>")
 
 set_km("n", "<Leader>a", ":Alpha<CR>")
@@ -38,5 +49,10 @@ set_km("n", "<Leader>s", ":wa!<CR>")
 
 set_km("n", "c*", "/<C-R>=expand('<cword>')<CR>/C<CR>``cgn")
 set_km("n", "c#", "/<C-R>=expand('<cword>')<CR>/C<CR>``cgN")
+
+set_km("n", "<Leader>h", "<C-w><C-h>")
+set_km("n", "<Leader>j", "<C-w><C-j>")
+set_km("n", "<Leader>k", "<C-w><C-k>")
+set_km("n", "<Leader>l", "<C-w><C-l>")
 
 set_km("t", "<C-o>", "<C-\\><C-n>")
