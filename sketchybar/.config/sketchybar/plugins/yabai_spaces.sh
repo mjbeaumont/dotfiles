3a69cb07ef9f934853241934f47a8e25183bce3a
Matthew Beaumont <matthewbeaumont@Matthews-MacBook-Pro-2.local>
2022-05-28 12:14:46 -0400

    fixed for yabai

--- sketchybar/.config/sketchybar/plugins/yabai_spaces.sh
+++ sketchybar/.config/sketchybar/plugins/yabai_spaces.sh
@@ -4,18 +4,10 @@ sketchybar --add       space              space_template left                \
                                           label.drawing=off                  \
                                           drawing=off                        \
                                           updates=on                         \
-                                          label.font="$FONT:Black:13.0"      \
                                           icon.font="$FONT:Bold:17.0"        \
                                           script="$PLUGIN_DIR/space.sh"      \
                                           click_script="$SPACE_CLICK_SCRIPT" \
                                                                              \
-           --clone     spaces_1.label     label_template                     \
-           --set       spaces_1.label     label=spc                          \
-                                          label.width=52                     \
-                                          label.align=left                 \
-                                          associated_display=1               \
-                                          position=left                      \
-                                          drawing=on                         \
 
 icons=("􀃉" "􀤊" "􀓨" "􀆪" "􀙚" "􀃓" "􀃕" "􀃗" "􀃙" "􀃛" "􀕒" "􀕓" "􀕔" "􀕕" "􀕖") #0 to 14
 highlight_colors=("" "0xff9dd274" "0xfff69c5e" "0xff72cce8" "0xffeacb64" "0xff9dd274" "0xfff69c5e" "0xff72cce8" "0xffeacb64" "0xff9dd274" "0xfff69c5e" "0xff72cce8" "0xffeacb64" "0xff9dd274" "0xfff69c5e")
@@ -37,8 +29,7 @@ do
   
   NAMES="$NAMES $NAME"
   args+=(--clone "$NAME" space_template after \
-    --set "$NAME" label="${label}" \
-    icon="${icons[${index}]}" \
+    --set "$NAME" icon="${icons[${index}]}" \
     icon.highlight_color="${highlight_colors[${index}]}" \
     associated_space=${index}
     associated_display=${display} \
@@ -47,4 +38,6 @@ do
 done <<< "$QUERY"
 
 args+=(--reorder $NAMES)
-sketchybar -m ${args[@]}
+sketchybar -m ${args[@]} \
+           --add bracket yabai_spaces \
+           ${NAMES} \
