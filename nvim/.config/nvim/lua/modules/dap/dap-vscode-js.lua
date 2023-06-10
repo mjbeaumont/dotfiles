714c71e3c7d291c276b2be91c39f4f319da14c3b
Matthew Beaumont <matthewbeaumont@Matthews-MacBook-Pro-2.local>
2023-06-10 17:16:54 -0400

    got node debugger to work

---
+++ nvim/.config/nvim/lua/modules/dap/dap-vscode-js.lua
@@ -0,0 +1,8 @@
+local status_ok, dap_vscode_js = pcall(require, "dap-vscode-js")
+if not status_ok then
+    return
+end
+
+dap_vscode_js.setup({
+    adapters = { "pwa-node", "pwa-chrome",  "node-terminal", },
+})
