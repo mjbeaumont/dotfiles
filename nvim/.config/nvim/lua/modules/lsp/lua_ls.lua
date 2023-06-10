100e6c2b1e2af440f02afc8c0c1fc82d5337a305
Matthew Beaumont <matthewbeaumont@Matthews-MacBook-Pro-2.local>
2023-06-10 16:16:52 -0400

    fix lua lsp settings

--- nvim/.config/nvim/lua/modules/lsp/lua_ls.lua
+++ nvim/.config/nvim/lua/modules/lsp/lua_ls.lua
@@ -2,20 +2,22 @@ local M = {}
 
 M.getOpts = function()
     return {
-        Lua = {
-            runtime = {
-                version = "LuaJIT",
-            },
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
+        settings = {
+            Lua = {
+                runtime = {
+                    version = "LuaJIT",
+                },
+                diagnostics = {
+                    globals = { "vim" },
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
             },
         },
     }
