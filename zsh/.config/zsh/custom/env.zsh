d1c82940173494f3570f4d3ceccc8e1ded37fd51
Matt Beaumont <mbeaumont@thefarmersdog.com>
2023-06-15 12:14:35 -0400

    update path and nvm script

--- zsh/.config/zsh/custom/env.zsh
+++ zsh/.config/zsh/custom/env.zsh
@@ -1,8 +1,8 @@
-export PATH=$HOME/bin/:/usr/local/bin:$HOME/Library/Python/2.7/bin:$PATH
+export PATH=/opt/homebrew/bin:$HOME/bin/:/usr/local/bin:$HOME/Library/Python/2.7/bin:$PATH
 export NVM_DIR="$HOME/.nvm"
 export FZF_CTRL_T_COMMAND='ag -l --nocolor --hidden  -g ""'
 export FZF_DEFAULT_COMMAND='ag -l --nocolor --hidden  -g ""'
-. "/usr/local/opt/nvm/nvm.sh"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 export TZ=America/New_York
 export LESS='-R'
 export LESSOPEN='|~/.lessfilter %s'
