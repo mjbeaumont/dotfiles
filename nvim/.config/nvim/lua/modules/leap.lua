5424c6f87f50cc65bb2e4f193411db67588d2a45
Matt Beaumont <mbeaumont@thefarmersdog.com>
2023-06-15 22:14:47 -0400

    fixed leap.nvim

--- nvim/.config/nvim/lua/modules/leap.lua
+++ nvim/.config/nvim/lua/modules/leap.lua
@@ -4,24 +4,19 @@ if not status_ok then
 end
 
 leap.setup({
-    case_insensitive = true,
+    case_sensitive = true,
     -- These keys are captured directly by the plugin at runtime.
     special_keys = {
         repeat_search = "<enter>",
-        next_match = "<enter>",
-        prev_match = "<tab>",
+        next_target = "<enter>",
+        prev_target = "<tab>",
         next_group = "<space>",
         prev_group = "<tab>",
-        eol = "<space>",
     },
 })
 
-leap.set_default_keymaps()
+leap.add_default_mappings()
 
 function _G.LeapBidirectional()
-    leap.leap({
-        ["target-windows"] = {
-            vim.api.nvim_get_current_win(),
-        },
-    })
+  leap.leap { target_windows = {vim.api.nvim_get_current_win()} }
 end
