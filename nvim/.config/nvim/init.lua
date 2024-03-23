c9fe8056bc32f01c073299bcc5551a086da2ede7
Matt Beaumont <mbeaumont@thefarmersdog.com>
2024-03-23 14:14:22 -0400

    moved diagnostics into lazy module

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,7 +4,6 @@ require("keymaps")
 require("options")
 require("lazy_nvim")
 require("modules/dap")
-require("modules/nvim-trouble")
 
 vim.cmd([[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]])
 vim.cmd([[autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab]])
