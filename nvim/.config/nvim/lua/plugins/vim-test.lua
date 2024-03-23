4feb03e5235a10b36bf95e001c35092ae5b48eb6
Matt Beaumont <mbeaumont@thefarmersdog.com>
2024-03-23 11:48:55 -0400

    moved vim-test to lazy module

---
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,55 @@
+return {
+    "vim-test/vim-test",
+    lazy = true,
+    dependencies = { "voldikss/vim-floaterm" },
+    config = function()
+        vim.g["test#javascript#jest#file_pattern"] = "\\vtest\\.(js|jsx|ts|tsx)$"
+        vim.g["test#javascript#jest#executable"] = "NODE_ENV=test node_modules/.bin/jest"
+        vim.cmd('let g:test#enabled_runners = ["javascript#jest"]')
+        vim.g["test#strategy"] = "floaterm"
+
+        local function getJestCwd()
+            local lsputil = require("lspconfig.util")
+
+            -- path of the current buffer, relative to the cwd
+            local currentBufferFilePath = vim.fn.expand("%:~:.")
+
+            local pkgJsonParentDir = lsputil.find_package_json_ancestor(currentBufferFilePath)
+            return pkgJsonParentDir
+        end
+
+        _G.setJestCwd = function()
+            vim.g["test#project_root"] = getJestCwd()
+        end
+
+        vim.cmd([[
+          augroup test
+          autocmd!
+          autocmd BufEnter *.tsx,*.ts,*.js,*.jsx call v:lua.setJestCwd()
+          augroup END
+        ]])
+    end,
+    keys = {
+
+        {
+            "<Leader>tf",
+            ":TestFile<CR>",
+            desc = "Run all tests in file",
+        },
+        {
+            "<Leader>tn",
+            ":TestNearest<CR>",
+            desc = "Run nearest test",
+        },
+        {
+            "<Leader>ts",
+            ":TestSuite<CR>",
+            desc = "Run test suite",
+        },
+        {
+            "<Leader>tl",
+            ":TestLast<CR>",
+            desc = "Run last test",
+        },
+    },
+}
