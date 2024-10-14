f13bac0d4c8aa4cf1349178caf5b002d39fef110
Matt Beaumont <mbeaumont@thefarmersdog.com>
2024-10-14 11:48:56 -0400

    configure surround plugin so it works

--- nvim/.config/nvim/lua/plugins/motions.lua
+++ nvim/.config/nvim/lua/plugins/motions.lua
@@ -2,7 +2,16 @@ return {
     "tpope/vim-abolish",
     "tpope/vim-commentary",
     "tpope/vim-repeat",
-    "kylechui/nvim-surround",
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end,
+    },
     "wellle/targets.vim",
     "moll/vim-bbye",
     {
