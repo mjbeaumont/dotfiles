a98349197eb8f0ab974f3d1a2d47eb90ece5a281
Matt Beaumont <mbeaumont@thefarmersdog.com>
2025-11-23 10:23:31 -0500

    added json validation

--- nvim/.config/nvim/lsp/jsonls.lua
+++ nvim/.config/nvim/lsp/jsonls.lua
@@ -3,8 +3,21 @@ local capabilities = require("core/lsp/capabilities")
 return {
     settings = {
         json = {
-            schemas = require("schemastore").json.schemas(),
-            validate = { enable = true },
+            schemas = require("schemastore").json.schemas({
+                {
+                    fileMatch = { "**/package.json" },
+                    url = "https://json.schemastore.org/package.json",
+                },
+                {
+                    fileMatch = { "**/tsconfig*.json" },
+                    url = "https://json.schemastore.org/tsconfig.json",
+                },
+                {
+                    fileMatch = { "**/jest.config.json" },
+                    url = "https://json.schemastore.org/jest.json",
+                },
+            }),
+            validate = true,
         },
     },
     capabilities = capabilities,
