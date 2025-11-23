d742446631194406c7939bd089ec5d4bfc2d2c72
Matt Beaumont <mbeaumont@thefarmersdog.com>
2025-11-23 10:11:15 -0500

    updated autocommand to use lua

--- nvim/.config/nvim/lua/core/autocommands.lua
+++ nvim/.config/nvim/lua/core/autocommands.lua
@@ -1,7 +1,26 @@
-vim.cmd([[
-  augroup _general_settings
-    autocmd!
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
-    autocmd FileType qf set nobuflisted
-  augroup end
-]])
+local api = vim.api
+
+local general = api.nvim_create_augroup("_general_settings", { clear = true })
+
+-- highlight on yank
+api.nvim_create_autocmd("TextYankPost", {
+    group = general,
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+    end,
+})
+
+-- disable folding 
+local nofold = vim.api.nvim_create_augroup("nofold_files", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = nofold,
+    pattern = { "json", "jsonc", "yaml", "yml" },
+    callback = function()
+        -- Disable folding completely
+        vim.opt_local.foldenable = false
+        vim.opt_local.foldmethod = "manual"
+        vim.opt_local.foldexpr = "0"
+    end,
+})
+
