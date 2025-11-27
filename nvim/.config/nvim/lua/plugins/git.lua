4aead54bb4944624d16c3075975b900f7cfbd257
Matt Beaumont <mbeaumont@thefarmersdog.com>
2025-11-26 20:57:39 -0500

    custom command to disable git blame temporarily when diagnostics available

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -12,16 +12,43 @@ return {
 
             gitsigns.setup({
                 sign_priority = 100,
-                current_line_blame = true,
+                current_line_blame = false, -- controlled manually
                 current_line_blame_opts = {
                     delay = 200,
                     virt_text_pos = "eol",
                 },
                 current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
             })
+
+            -- Toggle blame based on diagnostics
+            local blame_enabled = true
+            local function update_blame()
+                if not blame_enabled then return end
+                local line = vim.fn.line(".") - 1
+                local diagnostics = vim.diagnostic.get(0, { lnum = line })
+                if #diagnostics > 0 then
+                    gitsigns.toggle_current_line_blame(false)
+                else
+                    gitsigns.toggle_current_line_blame(true)
+                end
+            end
+
+            vim.api.nvim_create_autocmd({ "CursorHold", "DiagnosticChanged" }, {
+                callback = update_blame,
+            })
+
+            -- Expose toggle that respects our logic
+            vim.api.nvim_create_user_command("GitBlameToggle", function()
+                blame_enabled = not blame_enabled
+                if not blame_enabled then
+                    gitsigns.toggle_current_line_blame(false)
+                else
+                    update_blame()
+                end
+            end, {})
         end,
         keys = {
-            { "<Leader>gbt", ":Gitsigns toggle_current_line_blame<CR>", desc = "Toggle Line Blame", silent = true },
+            { "<Leader>gbt", ":GitBlameToggle<CR>", desc = "Toggle Line Blame", silent = true },
         },
     },
     {
