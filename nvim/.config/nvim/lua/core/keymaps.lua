3f2a53fddf39f02b96288cc9a2aae0b83502434b
Matt Beaumont <mbeaumont@thefarmersdog.com>
2025-11-26 21:57:23 -0500

    some small keymap changes

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,52 +1,66 @@
-local set_km = require("modules/utils")
+local function map(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
 
 --Remap space as leader key
-set_km("", "<Space>", "<Nop>")
+map("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- s + any two keys to jump around document
--- set_km("n", "s", "<Plug>(easymotion-s2)", { noremap = false })
+map("n", "Q", "<Nop>")
 
-set_km("n", "Q", "<Nop>")
+-- Center cursor after jumps
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Join lines without cursor moving
+map("n", "J", "mzJ`z")
 
 -- shortcut to jump to beginning/end of line
-set_km("n", "H", "^")
-set_km("n", "L", "$")
+map({"n", "v", "o"}, "H", "^")
+map({"n", "v", "o"}, "L", "$")
 
-set_km("n", "<C-j>", ":bprev<CR>")
-set_km("n", "<C-k>", ":bnext<CR>")
+map("n", "<C-j>", ":bprev<CR>")
+map("n", "<C-k>", ":bnext<CR>")
 
-set_km("n", "<Leader>p", "_dP")
-set_km("n", "<Leader>sv", ":lua ReloadConfig()<CR>")
+-- Visual paste without overwriting register
+-- (uses whatever register you specify, e.g., p, "+p, "0p all work)
+map("v", "p", function()
+    return '"_d"' .. vim.v.register .. 'P'
+end, { expr = true })
+map("n", "<Leader>sv", ":lua ReloadConfig()<CR>")
 
 -- easy move selected text
-set_km("v", "J", ":m '>+1<CR>gv=gv")
-set_km("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
-set_km("v", ">", ">gv")
-set_km("v", "<", "<gv")
+map("v", ">", ">gv")
+map("v", "<", "<gv")
 
 -- easy resize windows
-set_km("n", "<C-Up>", ":resize -2<CR>")
-set_km("n", "<C-Down>", ":resize +2<CR>")
-set_km("n", "<C-Left>", ":vertical resize -2<CR>")
-set_km("n", "<C-Right>", ":vertical resize +2<CR>")
+map("n", "<C-Up>", ":resize -2<CR>")
+map("n", "<C-Down>", ":resize +2<CR>")
+map("n", "<C-Left>", ":vertical resize -2<CR>")
+map("n", "<C-Right>", ":vertical resize +2<CR>")
 
-set_km("n", "<C-n>", ":lua ToggleNumber()<CR>")
-set_km("n", "yp", ":let @*=expand('%')<CR>")
+map("n", "<C-n>", ":lua ToggleNumber()<CR>")
+map("n", "yp", ":let @*=expand('%')<CR>")
 
-set_km("n", "<Leader>w", ":Bdelete<CR>")
-set_km("n", "<Leader>kw", ":bufdo :Bdelete<CR>")
-set_km("n", "<Leader>q", ":qa!<CR>")
-set_km("n", "<Leader>s", ":wa!<CR>")
+map("n", "<Leader>w", ":Bdelete<CR>")
+map("n", "<Leader>kw", ":bufdo :Bdelete<CR>")
+map("n", "<Leader>q", ":qa!<CR>")
+map("n", "<Leader>s", ":wa!<CR>")
 
-set_km("n", "c*", "/<C-R>=expand('<cword>')<CR>/C<CR>``cgn", { desc = "Change current word (repeatable forward)" })
-set_km("n", "c#", "/<C-R>=expand('<cword>')<CR>/C<CR>``cgN", { desc = "Change current word (repeatable backward)" })
+map("n", "c*", "/<C-R>=expand('<cword>')<CR>/C<CR>``cgn", { desc = "Change current word (repeatable forward)" })
+map("n", "c#", "/<C-R>=expand('<cword>')<CR>/C<CR>``cgN", { desc = "Change current word (repeatable backward)" })
 
-set_km("n", "<Leader>h", "<C-w><C-h>")
-set_km("n", "<Leader>j", "<C-w><C-j>")
-set_km("n", "<Leader>k", "<C-w><C-k>")
-set_km("n", "<Leader>l", "<C-w><C-l>")
+map("n", "<Leader>h", "<C-w><C-h>")
+map("n", "<Leader>j", "<C-w><C-j>")
+map("n", "<Leader>k", "<C-w><C-k>")
+map("n", "<Leader>l", "<C-w><C-l>")
 
-set_km("t", "<C-o>", "<C-\\><C-n>")
+map("t", "<C-o>", "<C-\\><C-n>")
